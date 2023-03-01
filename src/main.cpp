3ea17a4aac54574f6ee5344191c96f5260a5767d
amlanstudio <sarah.ngotta@yahoo.fr>
2023-03-01 22:47:55 +0100

    Quelques petites fonctions
    > Une classe circle avec des attributs (positions et rayon) + méthode (vérifications de la surface du canva)
    
    > pb sur mes contraintes qui ne sont pas respectée
    
    > implémentation d'une fonction drawCircle, elle fonctionne

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,84 @@
-#include <cstdlib>
 #include "p6/p6.h"
+#include <cstdlib>
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest/doctest.h"
 
-int main(int argc, char* argv[])
-{
-    { // Run the tests
-        if (doctest::Context{}.run() != 0)
-            return EXIT_FAILURE;
-        // The CI does not have a GPU so it cannot run the rest of the code.
-        const bool no_gpu_available = argc >= 2 && strcmp(argv[1], "-nogpu") == 0; // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-        if (no_gpu_available)
-            return EXIT_SUCCESS;
+// My classes
+
+class circle {
+public:
+  circle(p6::Context &ctx) {
+    xPosition = p6::random::number();
+    yPosition = p6::random::number();
+    radius = 0.2f;
+
+    leftLimit = ctx.current_canvas_width() + 2 * radius;
+    rightLimit = ctx.current_canvas_width() - 2 * radius;
+    topLimit = ctx.current_canvas_height() + 2 * radius;
+    bottomLimit = ctx.current_canvas_height() - 2 * radius;
+  }
+
+  bool canvasBorders() {
+    if (xPosition < static_cast<float>(leftLimit) &&
+        xPosition > static_cast<float>(rightLimit) &&
+        yPosition<static_cast<float>(topLimit) & yPosition> static_cast<float>(
+            bottomLimit)) {
+      return false;
     }
+    return true;
+  }
 
-    // Actual app
-    auto ctx = p6::Context{{.title = "Simple-p6-Setup"}};
-    ctx.maximize_window();
+  float xPosition;
+  float yPosition;
+  float radius;
 
-    // Declare your infinite update loop.
-    ctx.update = [&]() {
-        ctx.background(p6::NamedColor::Blue);
-        ctx.circle(
-            p6::Center{ctx.mouse()},
-            p6::Radius{0.2f}
-        );
-    };
+private:
+  int leftLimit;
+  int rightLimit;
+  int topLimit;
+  int bottomLimit;
+};
 
-    // Should be done last. It starts the infinite loop.
-    ctx.start();
+/*-------------My functions ------------*/
+
+void drawCircle(p6::Context &ctx, circle myCircle) {
+  ctx.fill = {p6::NamedColor::Salmon};
+  ctx.circle(p6::Center{myCircle.xPosition, myCircle.yPosition},
+             p6::Radius{myCircle.radius});
+};
+
+// ------------------
+
+int main(int argc, char *argv[]) {
+  { // Run the tests
+    if (doctest::Context{}.run() != 0)
+      return EXIT_FAILURE;
+
+    // The CI does not have a GPU so it cannot run the rest of the code.
+    const bool no_gpu_available =
+        argc >= 2 &&
+        strcmp(argv[1], "-nogpu") ==
+            0; // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+    if (no_gpu_available) {
+      return EXIT_SUCCESS;
+    }
+  }
+
+  // Actual app
+  auto ctx = p6::Context{{.title = "Simple-p6-Setup"}};
+  ctx.maximize_window();
+
+  // Declare your infinite update loop.
+  ctx.update = [&]() {
+    ctx.background({p6::NamedColor::FloralWhite});
+    // ctx.fill = {p6::NamedColor::Salmon};
+    ctx.use_stroke = false;
+
+    circle myCircle(ctx);
+    myCircle.canvasBorders();
+    drawCircle(ctx, myCircle);
+  };
+
+  // Should be done last. It starts the infinite loop.
+  ctx.start();
 }
